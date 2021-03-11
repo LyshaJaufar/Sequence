3352c54749abeddeb390c21339606fc3da288fb8
LyshaJaufar <lysha.ahmd@gmail.com>
2021-03-11 06:06:38 +0500

    upd

--- test.py
+++ test.py
@@ -1,25 +1,13 @@
-mylist = [1, 2, 3, 4, 5]
+def function(input):
+    global startValue, commonDifference
+    if input == 0:
+        return startValue
 
+    else:
+        return commonDifference + function(input-1)
 
 startValue = int(input("Enter the starting value: "))
 lengthofsequence = int(input("Enter the length of the sequence: "))
 commonDifference = int(input("Enter the pattern rule: "))
-newlist = [startValue]
+function(12)
 
-prev = startValue
-for i in range(lengthofsequence):
-    newlist.append(prev + commonDifference)
-    prev += commonDifference
-
-print(*newlist)
-
-
-# how to get the nth value
-newlist[0] = 5
-newlist[nthvalue] = newlist[nthvalue - 1] + commonDifference
-
-
-
-# what happens if the nth term doesn't exist yet?
-for i in range(n):
-    
